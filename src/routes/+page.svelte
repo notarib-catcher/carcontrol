300bbdfb6d7f836c54093b619db84eaf87b0cca8
notarib-catcher <15999680+notarib-catcher@users.noreply.github.com>
2024-10-14 00:25:04 +0530

    Added login logout test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script>
+    import { signIn, signOut } from '@auth/sveltekit/client'
+    import { page } from '$app/stores';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={signIn('google')}>
+    Signin
+</button>
+<button on:click={signOut()}>
+    Signout
+</button>
+
+{#if $page.data.session}
+{$page.data.session.user.email}
+{/if}
