044fb3c761293f4e625e845574bc36d3416cff6b
notarib-catcher <15999680+notarib-catcher@users.noreply.github.com>
2024-10-14 12:31:09 +0530

    Add tailwindcss

---
+++ src/lib/account-button.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { signIn, signOut } from '@auth/sveltekit/client'
+    import { page } from '$app/stores';
+</script>
+
+<div>
+    <div>
+        {#if $page.data.session?.user}
+            Hello, {$page.data.session.user.name}
+        {/if}
+    </div>
+    <button class=" border-b-2  rounded-md bg-slate-400 p-2" on:click={() => {
+        if($page.data.session?.user){
+            signOut()
+        }
+        else{
+            signIn('google')
+        }
+    }}>
+        {#if $page.data.session?.user}
+            Logout
+        {:else}
+            Login
+        {/if}
+    </button>
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,21 @@
     import { signIn, signOut } from '@auth/sveltekit/client'
     import { page } from '$app/stores';
     import { browser } from '$app/environment';
+    import AccountButton from '$lib/account-button.svelte';
 
     if(browser){
         console.log($page.data.session)
     }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<button on:click={async => {signIn('google')}}>
-    Signin
-</button>
-<button on:click={async => {signOut()}}>
-    Signout
-</button>
-
 {#if $page.data.session?.user}
 Logged in as: {$page.data.session.user.email}
 {:else}
 No session
 {/if}
+
+<p class=" text-red-500 text-3xl">
+    HELLO WORLD
+</p>
+
+<AccountButton />
